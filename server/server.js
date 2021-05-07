75b66fe7f8eeebaa49c794c869bfdeab622c861f
Mark Christian <m@rkchristian.ca>
2021-05-07 16:20:59 -0700

    Job serving; needs a claiming system

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require("express");
 require('express-async-errors');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 const path = require("path");
 const getDatabaseConnection = require("./lib/db");
 
@@ -8,7 +12,6 @@ const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8000;
 
 app.use(express.static("dist"));	// parcel artifacts
 app.use(express.static("public"));	// static files checked into git
-
 app.get("/", (req, res) => res.sendFile("../dist/index.html"));
 
 // Test endpoint
@@ -32,6 +35,64 @@ app.get("/db", async (req, res) => {
 	} catch {
 		res.send(JSON.stringify({ ok: false }));
 	}
+});
+
+const jobsCreateFields = upload.fields([
+	{
+		name: 'payload',
+		maxCount: 1,
+	},
+	{
+		name: 'code',
+		maxCount: 1,
+	},
+]);
+app.post("/jobs/create", jobsCreateFields, async (req, res) => {
+	res.header("Content-Type", "application/json");
+
+	console.log(req.files)
+	const payload = req.files.payload[0];
+	const code = req.files.code[0];
+	if (!code || !payload) throw new Error("Invalid job");
+
+	try {
+		const payloadHex = payload.buffer.toString('hex');
+		const codeHex = code.buffer.toString('hex');
+
+		console.log(`Creating job with ${payload.buffer.length} bytes of payload and ${code.buffer.length} bytes of code`);
+
+		const db = await getDatabaseConnection();
+		const resp = await db.query(
+			"insert into swasm_jobs (payload, code, start_ts, end_ts) values (decode($1, 'hex'), decode($2, 'hex'), 0, 0) returning id",
+			[payloadHex, codeHex]);
+		const jobId = resp.rows[0].id;
+		res.send(JSON.stringify({ ok: true, jobId }, null, "\t"));
+		console.log(`Created job #${jobId}`);
+	} catch(e) {
+		res.send(JSON.stringify({ ok: false, error: e.message }));
+	}
+});
+
+async function getJob(jobId) {
+	const db = await getDatabaseConnection();
+	const resp = await db.query("select * from swasm_jobs where id = $1", [ jobId ]);
+	return resp.rows[0];
+}
+
+app.get("/jobs/:jobId/code", async (req, res) => {
+	const { jobId } = req.params;
+
+	res.header("content-type", "application/wasm");
+	const job = await getJob(jobId);
+	res.send(job.code);
+});
+
+app.get("/jobs/:jobId/payload", async (req, res) => {
+	const { jobId } = req.params;
+
+	res.header("content-type", "application/octet-stream");
+	const job = await getJob(jobId);
+	res.send(job.payload);
 });
 
 app.listen(port, () => {
--- sql/0001-create-a-table.sql
+++ sql/0001-create-a-table.sql
@@ -1,3 +1,13 @@
-CREATE TABLE shinypbdb_example (
-	foo INT UNIQUE
+-- 
+-- Editor SQL for DB table swasm_jobs
+-- Created by http://editor.datatables.net/generator
+-- 
+
+CREATE TABLE IF NOT EXISTS swasm_jobs (
+	id serial,
+	payload bytea,
+	code bytea,
+	start_ts numeric(9,2),
+	end_ts numeric(9,2),
+	PRIMARY KEY( id )
 );
