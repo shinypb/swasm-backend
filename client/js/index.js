d505ed5f60cd4b605e8d32b446a3bd454f289158
Mark Christian <m@rkchristian.ca>
2021-05-12 15:34:25 -0700

    Run jobs forever

--- client/js/index.js
+++ client/js/index.js
@@ -10,26 +10,34 @@ async function claimJob() {
 	throw new Error(resp.error);
 }
 
-(async () => {
+(() => {
 	window.onhashchange = () => location.reload();
 
-	const jobId = await claimJob();
-	if (!jobId) {
-		console.error("[SCHEDULER] No jobs available");
-		return;
+	async function claimAndRunJob() {
+		const jobId = await claimJob();
+		if (!jobId) {
+			console.error("[SCHEDULER] No jobs available, will check again in a bit");
+			setTimeout(() => claimAndRunJob(), 10000);
+			return;
+		}
+
+		console.log("[SCHEDULER] Claimed job", jobId);
+
+		const result = await runJob({
+			jobId,
+			codeUrl: `/jobs/${jobId}/code`,
+			payloadUrl: `jobs/${jobId}/payload`,
+		});
+		const formData = new FormData();
+		formData.append("result", new Blob([result], { type: "application/octet-stream"} ));
+		const resp = await fetch(`/jobs/${jobId}/finish`, {
+			method: 'POST',
+			body: formData,
+		});
+
+		console.log("[SCHEDULER] Will look for another job shortly");
+		setTimeout(claimAndRunJob, 5000);
 	}
 
-	console.log("[SCHEDULER] Claimed job", jobId);
-
-	const result = await runJob({
-		jobId,
-		codeUrl: `/jobs/${jobId}/code`,
-		payloadUrl: `jobs/${jobId}/payload`,
-	});
-	const formData = new FormData();
-	formData.append("result", new Blob([result], { type: "application/octet-stream"} ));
-	const resp = await fetch(`/jobs/${jobId}/finish`, {
-		method: 'POST',
-		body: formData,
-	});
+	claimAndRunJob();
 })();
